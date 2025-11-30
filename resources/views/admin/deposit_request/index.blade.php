56f071bd8a7c11e254c57dac0bb3e2b9a5ce425e
AMK1215 <aungmyokhaing1215@gmail.com>
2025-11-30 14:55:41 +0630

    teacher wallet

--- resources/views/admin/deposit_request/index.blade.php
+++ resources/views/admin/deposit_request/index.blade.php
@@ -40,7 +40,7 @@
             <div class="row mb-2">
                 <div class="col-12">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+                        <li class="breadcrumb-item"><a href="{{ route('admin.home') }}">Home</a></li>
                         <li class="breadcrumb-item active">Deposit Request Lists</li>
                     </ol>
                 </div>
