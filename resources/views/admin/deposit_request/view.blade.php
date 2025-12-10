e0c63d06aaacb43991880bf2615e5f830c99caf6
AMK1215 <aungmyokhaing1215@gmail.com>
2025-12-10 13:21:57 +0630

    wallet system

--- resources/views/admin/deposit_request/view.blade.php
+++ resources/views/admin/deposit_request/view.blade.php
@@ -57,7 +57,7 @@
         <div class="row mb-2">
             <div class="col-12">
                 <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+                    <li class="breadcrumb-item"><a href="{{ route('admin.home') }}">Home</a></li>
                     <li class="breadcrumb-item active">Deposit Request Detail</li>
                 </ol>
             </div>
--- resources/views/admin/withdraw_request/view.blade.php
+++ resources/views/admin/withdraw_request/view.blade.php
@@ -46,7 +46,7 @@
         <div class="row mb-2">
             <div class="col-12">
                 <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+                    <li class="breadcrumb-item"><a href="{{ route('admin.home') }}">Home</a></li>
                     <li class="breadcrumb-item active">Withdraw Request Detail</li>
                 </ol>
             </div>
